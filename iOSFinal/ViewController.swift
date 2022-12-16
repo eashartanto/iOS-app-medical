f79c6bb995f4bab04a951650dfebb6d10a3d0fe0
ankitgusai <ankitgusai19@gmail.com>
2022-12-15 20:18:56 -0500

    Fb login working

--- iOSFinal/ViewController.swift
+++ iOSFinal/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import FacebookLogin
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        let loginButton = FBLoginButton()
+        loginButton.center = view.center
+        
+        // Extend the code sample from 6a. Add Facebook Login to Your Code
+        // Add to your viewDidLoad method:
+        loginButton.permissions = ["public_profile", "email"]
+          
+        view.addSubview(loginButton)
+        
+        if let token = AccessToken.current,
+                !token.isExpired {
+                // User is logged in, do work such as go to next view controller.
+                print("user already logged in ")
+            }
     }
 
-
 }
+
 
